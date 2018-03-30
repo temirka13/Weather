790bf7a5addfa03ca8276efc9ed99c7d81821b36
temirka13 <temirka13@mail.ru>
2018-03-30 22:06:50 +0300

    59

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,11 +6,13 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-    const url= `${ROOT_URL}&q=${city},ru`;
-    const requet = axios.get(url);
+    const url= `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    
 
     return {
         type: FETCH_WEATHER,
-        payload: requet
+        payload: request
     };
 }
